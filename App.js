d35ea2ba87777994b2950e7142ff4cf5b4c5340e
PriontoAbdullah <priontosoft@gmail.com>
2022-10-25 15:23:34 +0600

    Added initial home page

--- App.js
+++ App.js
@@ -1,28 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {
+const App = () => {
   return (
     <>
       <StatusBar style='auto' />
+
       <NavigationContainer>
-        <Stack.Navigator initialRouteName='Login'>
-          <Stack.Screen
-            name='Login'
-            component={LoginScreen}
-            options={{ title: 'Login to access this app!' }}
-          />
-          {/* <Stack.Screen name='Home' component={HomeScreen} /> */}
-          <Stack.Screen name='Home'>
-            {(props) => <HomeScreen {...props} extraData='Prionto' />}
-          </Stack.Screen>
+        <Stack.Navigator initialRouteName='Home'>
+          <Stack.Screen name='Home' component={HomeScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
   );
-}
+};
+
+export default App;
---
+++ screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import { Button, View, Text } from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View className='flex-1 items-center justify-center bg-white'>
+      <Text className='text-red-500 text-2xl'>Home Screen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
